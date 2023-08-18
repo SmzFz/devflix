6a15fb30e50dde54c6523ae6aefbf43c6bc29473
SmzFz <felipesalmazo123@gmail.com>
2023-08-18 09:28:52 -0300

    desenvolvido compomente movieCard adaptações no app.jsx criado arquivo moviecard.module.css falta código no moviecard.module.css

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,12 +1,21 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import logo from "../assets/devflix.png";
-import searchIcon from "../assets/search.svg";
+import React, { useEffect, useState } from "react";
+{
+  /*Import React*/
+}
 
 import "./App.css";
 
+import logo from "../assets/devflix.png";
+import searchIcon from "../assets/search.svg";
+import MovieCard from "../components/movieCard/movieCard";
+
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState(" ");
+  const [searchTerm, setSearchTerm] = useState("");
+  {
+    /*Initialize searchTerm with an empty string*/
+  }
+  const [movies, setMovies] = useState([]);
+
   const apiKey = "e4d577fa";
   const apiUrl = `http://www.omdbapi.com/?apikey=${apiKey}`;
 
@@ -18,12 +27,21 @@ const App = () => {
     const response = await fetch(`${apiUrl}&s=${title}`);
     const data = await response.json();
 
+    console.log(data);
+    {
+      /*You can use data to update your UI with movie information*/
+    }
+    setMovies(data.Search);
   };
 
   const handleKeyPress = (e) => {
-    e.Key === "Enter" && searchMovies(searchTerm)
-    searchMovies(searchTerm)
-  }
+    if (e.key === "Enter") {
+      {
+        /* Use lowercase "key"*/
+      }
+      searchMovies(searchTerm);
+    }
+  };
 
   return (
     <div id="app">
@@ -43,6 +61,17 @@ const App = () => {
           onClick={() => searchMovies(searchTerm)}
         />
       </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard key={movie.imdbID} movies={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>Nenhum filme encontrado😥</h2>
+        </div>
+      )}
     </div>
   );
 };
